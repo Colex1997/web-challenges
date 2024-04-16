bab517eb41754e158cf89a9f8cba2fac13b3a2e7
Alexander <alexander.niehaus@gmx.net>
2024-04-16 16:10:34 +0200

    solve second js structure challenge

---
+++ js-structure_quiz-app/index.js
@@ -0,0 +1,12 @@
+// Store the cards in a global state
+import { cards } from "./components/CardList/data.js";
+import { Header } from "./components/Header/header.js";
+import { Form, handleFormSubmit } from "./components/Form/createForm.js";
+import { Bookmark } from "./components/Bookmark/Bookmark.js";
+import { Card } from "./components/Card/Card.js";
+import { CardList } from "./components/CardList/CardList.js";
+import { App, render } from "./components/App/App.js";
+
+// The render function renders the app to the DOM
+
+render();
