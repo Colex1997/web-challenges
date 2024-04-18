036c933d299fca96f79067027b39f3f5f1acbf5c
Alexander <alexander.niehaus@gmx.net>
2024-04-18 15:32:36 +0200

    add fetch modifications

--- js-error-handling_fetch/js/index.js
+++ js-error-handling_fetch/js/index.js
@@ -1,16 +1,32 @@
 console.clear();
 
 const userElement = document.querySelector(".user");
+const errorMessage = document.querySelector("p");
 
 async function getUser(url) {
-  const response = await fetch(url);
-  const json = await response.json();
-  return json.data;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("response nich ok");
+    }
+    const json = await response.json();
+    return json.data;
+  } catch (error) {
+    console.log(response.status);
+    return null;
+  }
 }
 
 document.querySelectorAll("button[data-url]").forEach((button) =>
   button.addEventListener("click", async (event) => {
     const user = await getUser(event.target.dataset.url);
+    errorMessage.innerText = "";
+    if (!user) {
+      console.log("User not found");
+      userElement.innerHTML = "";
+      errorMessage.innerText = "User not found!";
+      return;
+    }
     userElement.innerHTML = `
     <h2>${user.first_name} ${user.last_name}</h2>
     <img alt="${user.first_name} ${user.last_name}" src="${user.avatar}"/>
