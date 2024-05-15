831ee58f254b03b64ac60c4e463f0fcd7a658644
Alexander <alexander.niehaus@gmx.net>
2024-05-15 10:55:03 +0200

    solve first backend api challenge

---
+++ backend-api-routes_products/pages/products/[id].js
@@ -0,0 +1,23 @@
+import { useRouter } from "next/router";
+import useSWR from "swr";
+
+export default function Product() {
+  const router = useRouter();
+  const { id } = router.query;
+
+  const { data, isLoading } = useSWR(`/api/products/${id}`);
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (!data) {
+    return;
+  }
+
+  return (
+    <>
+      <p>{data.name}</p>
+    </>
+  );
+}
---
+++ backend-api-routes_products/pages/products/index.js
@@ -0,0 +1,23 @@
+import useSWR from "swr";
+
+export default function ProductList() {
+  const { data, isLoading } = useSWR("/api/products");
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (!data) {
+    return;
+  }
+
+  console.log(data);
+
+  return (
+    <ul>
+      {data.map((product) => (
+        <li key={product.id}>{product.name}</li>
+      ))}
+    </ul>
+  );
+}
