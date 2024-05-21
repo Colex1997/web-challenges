ac03f83d42b77cfb25da3619c55a48e838f2fb44
Alexander <alexander.niehaus@gmx.net>
2024-05-21 10:43:55 +0200

    implement read and create of recap 6

---
+++ recap-project-6_tourio-app/pages/api/places/[id]/index.js
@@ -0,0 +1,20 @@
+import Place from "@/db/models/Place";
+import dbConnect from "@/db/connect";
+
+export default async function handler(request, response) {
+  await dbConnect();
+  const { id } = request.query;
+
+  if (!id) {
+    return;
+  }
+
+  if (request.method === "GET") {
+    console.log(id);
+    const place = await Place.findById(id);
+    if (!place) {
+      return response.status(404).json({ status: "Not found" });
+    }
+    response.status(200).json(place);
+  }
+}
---
+++ recap-project-6_tourio-app/pages/api/places/index.js
@@ -0,0 +1,21 @@
+import Place from "@/db/models/Place";
+import dbConnect from "@/db/connect";
+
+export default async function handler(request, response) {
+  await dbConnect();
+  if (request.method === "GET") {
+    const places = await Place.find();
+    return response.status(200).json(places);
+  }
+
+  if (request.method === "POST") {
+    try {
+      const placeData = request.body;
+      await Place.create(placeData);
+      response.status(201).json({ status: "Place created" });
+    } catch (error) {
+      console.error(error);
+      response.status(400).json({ error: error.message });
+    }
+  }
+}
